dfeaef8e61a8a6e88bae490a919b8e4bec295bce
Pere V.E <perevalls29@gmail.com>
2018-12-13 17:20:21 +0100

    afegir espadatxi i arquer

--- classes/main.cpp
+++ classes/main.cpp
@@ -6,21 +6,32 @@ class Guerrer {
 		Guerrer(int vida)
 		{
 			this->vida = vida;
+			
 		}
 		int mostraVida(){
 			return vida;
 		}
+		void ModificarVida(int mod){
+			vida += mod;
+			printf("Ara te %i vida",vida);
+		}
 	public:
 		int vida = 0;
+		int atac = 1;
 };
-
 class Arquer : public Guerrer {
 	
 };
 
+class Espadatxi : public Guerrer {
+	
+};
 int main(int argc, char** argv) {
-	Guerrer a(5);
 	Arquer b;
+	b.vida = 5;
+	Espadatxi c;
+	c.vida = 5;
+	b.ModificarVida(c.atac);
 	printf("%i",b.mostraVida());
 	return 0;
 }
