5c7fb2e1bd9ee9135ba5883d7b6939fc16fb08a7
Pere V.E <perevalls29@gmail.com>
2018-12-13 17:30:57 +0100

    afegir l'atac especial del espadatxi

--- classes/main.cpp
+++ classes/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 class Guerrer {
 	public:
@@ -13,25 +15,35 @@ class Guerrer {
 		}
 		void ModificarVida(int mod){
 			vida += mod;
-			printf("Ara te %i vida",vida);
+			printf("Ara te %i vides",vida);
 		}
 	public:
 		int vida = 0;
 		int atac = 1;
 };
 class Arquer : public Guerrer {
-	
+	public:
+		int atacFletxa = 3;
 };
 
 class Espadatxi : public Guerrer {
-	
+	public:
+		int atacEspecial(){
+			if (rand()%3 == 0){
+				printf("has fallat\n");
+				return 0;
+			}
+			else{
+				return 2;
+			}
+		}
 };
 int main(int argc, char** argv) {
+	srand(time(NULL));
 	Arquer b;
 	b.vida = 5;
 	Espadatxi c;
 	c.vida = 5;
-	b.ModificarVida(c.atac);
-	printf("%i",b.mostraVida());
+	b.ModificarVida(-c.atacEspecial());
 	return 0;
 }
